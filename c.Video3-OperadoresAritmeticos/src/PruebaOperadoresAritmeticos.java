4a2353eeed734e0a3d3a8944134ede6e6f930d24
Guillermo MZ <guillermo.mariscal.zaballa@gmail.com>
2025-03-10 17:54:21 +0100

    Video 3.
    Commit del Video 3. Operadores Aritméticos.

---
+++ c.Video3-OperadoresAritmeticos/src/PruebaOperadoresAritmeticos.java
@@ -0,0 +1,43 @@
+public class PruebaOperadoresAritmeticos
+{
+
+	public static void main(String[] args)
+	{
+		// suma, resta, multiplicación, división y resto.
+
+		// suma: +
+		// resta: -
+		// división: /
+		// resto: %
+
+		// int operandoUno = 10;
+		double operandoUno;
+		operandoUno = 15.248;
+		double operandoDos = 7.95;
+		double resultado = 0;
+
+		// Operaciones con variables y demo de los resultados
+
+		// suma
+		resultado = operandoUno + operandoDos;
+		System.out.println(resultado);
+
+		// resta
+		resultado = operandoUno - operandoDos;
+		System.out.println(resultado);
+
+		// multiplicación
+		resultado = operandoUno * operandoDos;
+		System.out.println(resultado);
+
+		// división
+		resultado = operandoUno / operandoDos;
+		System.out.println(resultado);
+
+		// resto
+		resultado = operandoUno % operandoDos;
+		System.out.println(resultado);
+
+	}
+
+}
