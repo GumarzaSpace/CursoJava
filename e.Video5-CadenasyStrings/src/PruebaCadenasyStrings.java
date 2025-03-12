91108686a602ba1ed15b7a4ec629b0d30523e850
Guillermo MZ <guillermo.mariscal.zaballa@gmail.com>
2025-03-12 18:03:12 +0100

    Video 5.
    Commit del Video 5. Cadenas y Strings.

---
+++ e.Video5-CadenasyStrings/src/PruebaCadenasyStrings.java
@@ -0,0 +1,49 @@
+
+public class PruebaCadenasyStrings
+{
+
+	public static void main(String[] args)
+	{
+		// char -> A
+		char caracter = 'A';
+		char caracter2 = 'v';
+		char caracter3 = 'i';
+		// Avión
+		
+		
+		// int numero;
+	    // numero = 5;		
+		String miCadena;
+		miCadena = "Avión";
+		miCadena = "Coche";
+		System.out.println(miCadena);
+		
+		
+		// El coche es azul
+		String cadena1 = "El ";
+		String cadena2 = "coche ";
+		String cadena3 = "es ";
+		String cadena4  = "azul";
+		
+		miCadena = "El coche es azul";
+		System.out.println(miCadena);
+		miCadena = cadena1 + cadena2 + cadena3 + cadena4;
+		System.out.println(miCadena);
+		
+		
+		// Imprimir mensajes deseados con información de feedback
+		int operando1 = 25;
+		int operando2 = 5;
+		int resultado = operando1 * operando2;
+		String resultadoFinal = "El resultado de la multiplicación de " + operando1 + " y " + operando2 + " es: " + resultado;
+		System.out.println(resultadoFinal);
+		System.out.println();
+		System.out.println("Mi mensaje");
+		
+		
+		
+		
+
+	}
+
+}
